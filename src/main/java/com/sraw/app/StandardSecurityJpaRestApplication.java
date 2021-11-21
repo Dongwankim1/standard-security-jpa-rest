79fa2af9ef4e79447b35d37fc617e713dc22e3a9
dongwan <zkdlwu94@naver.com>
2021-11-22 00:09:00 +0900

    first commit

---
+++ src/main/java/com/sraw/app/StandardSecurityJpaRestApplication.java
@@ -0,0 +1,13 @@
+package com.sraw.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardSecurityJpaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardSecurityJpaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sraw/app/StandardSecurityJpaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sraw.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardSecurityJpaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
